42757b1331e65d728dbe3a034f84a8e1fb4870f4
RichieDevDotExe <138896245+RichieDevDotExe@users.noreply.github.com>
2024-05-18 23:54:01 +0100

    trained

--- peopleTrackingV2.py
+++ peopleTrackingV2.py
@@ -3,6 +3,52 @@ import os
 import time
 from pathlib import Path
 from ultralytics.utils.plotting import save_one_box
+import torch 
+
+device = torch.device("cpu")
+
+
+def scanRoom():
+    model = YOLO('yolov8n.pt')
+    results = model.track(source="0",show=True, stream=True,classes=0)
+    timecheck = 0
+
+    startTime = time.time()
+    count = 0
+    for result in results:
+        names = model.names
+        peopleDetected = []
+        resArray = result.numpy()
+        print("\n=======================Results Process======================\n")
+        #print(resArray)
+        #result.save_crop("IDs")
+        if(type(resArray.boxes) != type(None)):
+            boxes = resArray.boxes
+            
+            if(type(boxes.id) != type(None)):
+                for i in range(0,len(boxes.id)):
+                    print("boxes- " + str(boxes.id)+ str(boxes.id.shape))
+                    folder = 'IDs'
+                    IDval = str(boxes.id[i])
+                    path = folder+"/"+IDval
+                    if not os.path.exists(path):
+                        print("Path no")
+                        os.mkdir(path)
+                        save_one_box(result.boxes.xyxy[i],resArray.orig_img.copy(),file=Path(path) / f"{Path(IDval)}.jpg",BGR=True)
+                    else:
+                        save_one_box(result.boxes.xyxy[i],resArray.orig_img.copy(),file=Path(path) / f"{Path(IDval)}.jpg",BGR=True)
+                    print("person " + str(boxes.id[i].item())+ " is in frame")
+                    
+            
+            for p in boxes.cls:
+                peopleDetected.append(int(p))
+
+            #function to set temperature with peopleDetected as input
+
+            timecheck = time.time()-startTime 
+        print("\n=======================IDs detected======================\n")
+        print(peopleDetected)
+    return peopleDetected
 
 def scanRoom():
     model = YOLO('yolov8n.pt')
@@ -51,10 +97,16 @@ def combineIDs():
     print("combine")
 
 def train():
-    model = YOLO('yolov8n.pt', device='gpu')
+    model = YOLO('yolov8n.pt')
+    model.to(device)
 
     results = model.train(data='Ubi-Comp-Me-2//data.yaml',epochs = 100, imgsz=640, device=0)
 
+def trainedScan():
+    model = YOLO('best.pt')
+    results = model.track(source="0",show=True, stream=True,classes=0)
 
 #scanRoom()
+
+trainedScan()
 
