299efd24981b363c2f04110624763ff1cdc1dfd7
lyra-scarlet <118722990+lyra-scarlet@users.noreply.github.com>
2024-05-18 21:02:39 +0100

    Include (dummy) target temperature in console output

--- peopleTrackingV2.py
+++ peopleTrackingV2.py
@@ -4,6 +4,35 @@ import time
 from pathlib import Path
 from ultralytics.utils.plotting import save_one_box
 
+# Dictionary stores preferred temperature in celsius
+tempPreferences = {
+    0: 19,
+    1: 16,
+    2: 22,
+    3: 21,
+    4: 17
+}
+# Baseline temperature, for if nobody is in the room
+baselineTemp = 15
+# function to decide a target temperature
+def getTargetTemp(inRoom):
+    # If nobody's in the room, keep it at baseline temperature
+    targetTemp = baselineTemp
+
+    # Otherwise, if somebody's in there, our target is the preference average
+    if inRoom:
+        targetTemp = 0
+        for person in inRoom:
+            # If the person is in the dictionary then use their preference 
+            if person in tempPreferences:
+                targetTemp += tempPreferences[person]
+            # Otherwise use baseline temperature
+            else:
+                targetTemp += baselineTemp
+        targetTemp /= len(inRoom)
+    
+    return targetTemp
+
 def scanRoom():
     model = YOLO('yolov8n.pt')
     results = model.track(source="0",show=True, stream=True,classes=0)
@@ -34,17 +63,22 @@ def scanRoom():
                     else:
                         save_one_box(result.boxes.xyxy[i],resArray.orig_img.copy(),file=Path(path) / f"{Path(IDval)}.jpg",BGR=True)
                     print("person " + str(boxes.id[i].item())+ " is in frame")
+                    peopleDetected.append(int(boxes.id[i]))
                     
             
-            for p in boxes.cls:
-                peopleDetected.append(int(p))
+            #for p in boxes.cls:
+            #    peopleDetected.append(int(p))
 
             #function to set temperature with peopleDetected as input
 
             timecheck = time.time()-startTime 
-        print("\n=======================IDs detected======================\n")
+        print("\n======IDs detected======\n")
         print(peopleDetected)
-    return peopleDetected
+
+        # Target temperature decided here
+        targetTemp = getTargetTemp(peopleDetected)
+        print("Target temperature: "+str(targetTemp))
+    #return peopleDetected
 
 def combineIDs():
     #hello
@@ -56,5 +90,5 @@ def train():
     results = model.train(data='Ubi-Comp-Me-2//data.yaml',epochs = 100, imgsz=640, device=0)
 
 
-#scanRoom()
+scanRoom()
 
